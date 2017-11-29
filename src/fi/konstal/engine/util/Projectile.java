0565ee88e8fe7c152b626a1d5c4e245360ce82fb
KonstaL <lehtinen.konsta@gmail.com>
2017-11-29 17:49:39 +0200

    Add projectile class

---
+++ src/fi/konstal/engine/util/Projectile.java
@@ -0,0 +1,25 @@
+package fi.konstal.engine.util;
+
+import fi.konstal.engine.GameObject;
+import javafx.scene.canvas.Canvas;
+
+/**
+ * Created by konka on 29.11.2017.
+ */
+public class Projectile extends GameObject {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void getInput() {
+
+    }
+
+    @Override
+    public void update(Canvas c) {
+
+    }
+}
